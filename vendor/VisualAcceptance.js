70dee76e6b3cc589a6b51cacf9b792e42d039929
White, Eric <ewhite@ciena.com>
2016-07-12 14:00:47 -0400

    refactor visual acceptance

--- vendor/VisualAcceptance.js
+++ vendor/VisualAcceptance.js
@@ -43,7 +43,9 @@ function capture (imageName, width, height, misMatchPercentageMargin, assert) {
   // resemble.outputSettings({
   //   largeImageThreshold: 0
   // })
-
+  return capturePhantom(imageName, width, height, misMatchPercentageMargin, assert, browserDirectory)
+}
+function capturePhantom (imageName, width, height, misMatchPercentageMargin, assert, browserDirectory) {
   return new Promise(function (resolve, reject) {
     if (window.callPhantom === undefined) {
       resolve('Not on PhantomJS')
@@ -54,99 +56,102 @@ function capture (imageName, width, height, misMatchPercentageMargin, assert) {
     })
     image = 'data:image/png;base64,' + image
       // console.log(image)
-    if (!document.getElementById('visual-acceptance') && $('.tabs').length === 0) {
-      var visualAcceptanceContainer
-      visualAcceptanceContainer = document.createElement('div')
-      visualAcceptanceContainer.setAttribute('id', 'visual-acceptance')
-      visualAcceptanceContainer.innerHTML = '<div class="title"> Visual Acceptance tests: </div> <div class="visual-acceptance-container"> </div>'
-      document.body.appendChild(visualAcceptanceContainer)
-    }
-    var node = document.createElement('div')
-    var images = []
+    return utilizeImage(imageName, width, height, misMatchPercentageMargin, assert, image, browserDirectory, resolve, reject)
+  })
+}
+function utilizeImage (imageName, width, height, misMatchPercentageMargin, assert, image, browserDirectory, resolve, reject) {
+  if (!document.getElementById('visual-acceptance') && $('.tabs').length === 0) {
+    var visualAcceptanceContainer
+    visualAcceptanceContainer = document.createElement('div')
+    visualAcceptanceContainer.setAttribute('id', 'visual-acceptance')
+    visualAcceptanceContainer.innerHTML = '<div class="title"> Visual Acceptance tests: </div> <div class="visual-acceptance-container"> </div>'
+    document.body.appendChild(visualAcceptanceContainer)
+  }
+  var node = document.createElement('div')
+  var images = []
     // Get passed image
-    var res = JSON.parse(httpGet('/image?name=' + encodeURIComponent(browserDirectory + imageName) + '-passed.png'))
-    if (res.error === 'File does not exist') {
-      // Save image as passed if no existing passed image
-      $.ajax({
-        type: 'POST',
-        async: false,
-        url: '/passed',
-        data: {
-          image: image,
-          name: browserDirectory + imageName + '.png'
-        }
-      })
-      $(document.getElementById('ember-testing')).removeAttr('style')
-      $(document.getElementById('ember-testing-container')).removeAttr('style')
-      node.innerHTML = '<div class="test pass"> <div class="list-name"> No new image. Saving current as baseline: ' + imageName + '</div> <div class="additional-info"> Addition Information: </div> <img src="' + image + '" /> </div>'
-      images.push(image)
-      $.ajax({
-        type: 'POST',
-        async: false,
-        url: '/report',
-        data: {
-          type: 'New',
-          images: images,
-          name: imageName
-        }
-      })
-      resolve('No passed image. Saving current test as base')
-    } else {
-      // Passed image exists so compare to current
-      res.image = 'data:image/png;base64,' + res.image
-      return new Promise(function (resolve, reject) {
-        resemble(res.image).compareTo(image).scaleToSameSize().onComplete(function (data) {
-          var result = false
-          if (parseFloat(data.misMatchPercentage) <= misMatchPercentageMargin) {
-            // Passed
-            $.ajax({
-              type: 'POST',
-              async: false,
-              url: '/passed',
-              data: {
-                image: image,
-                name: browserDirectory + imageName + '.png'
-              }
-            })
-            result = true
-            node.innerHTML = '<div class="test pass"> <div class="list-name">  New: ' + imageName + '</div> <div class="additional-info"> Addition Information: </div> <img src="' + image + '" /> </div>'
-          } else {
-            // Fail
-            $.ajax({
-              type: 'POST',
-              async: false,
-              url: '/fail',
-              data: {
-                image: data.getImageDataUrl(),
-                name: browserDirectory + imageName + '.png'
-              }
-            })
-            node.innerHTML = '<div class="test fail"> <div class="list-name">  Changed: ' + imageName + ' </div> <div class="additional-info"> Addition Information: </div> <div class="images"> <div class="image"> <img class="diff" src="' + data.getImageDataUrl() + '" /> <div class="caption">  Diff   </div> </div> <div class="image">  <img class="input" src="' + image + '" /> <div class="caption"> Current  </div> </div> <div class="image"> <img class="passed" src="' + res.image + '" /> <div class="caption"> Baseline   </div> </div> </div> </div>'
+  var res = JSON.parse(httpGet('/image?name=' + encodeURIComponent(browserDirectory + imageName) + '-passed.png'))
+  if (res.error === 'File does not exist') {
+    // Save image as passed if no existing passed image
+    $.ajax({
+      type: 'POST',
+      async: false,
+      url: '/passed',
+      data: {
+        image: image,
+        name: browserDirectory + imageName + '.png'
+      }
+    })
+    $(document.getElementById('ember-testing')).removeAttr('style')
+    $(document.getElementById('ember-testing-container')).removeAttr('style')
+    node.innerHTML = '<div class="test pass"> <div class="list-name"> No new image. Saving current as baseline: ' + imageName + '</div> <div class="additional-info"> Addition Information: </div> <img src="' + image + '" /> </div>'
+    images.push(image)
+    $.ajax({
+      type: 'POST',
+      async: false,
+      url: '/report',
+      data: {
+        type: 'New',
+        images: images,
+        name: imageName
+      }
+    })
+    resolve('No passed image. Saving current test as base')
+  } else {
+    // Passed image exists so compare to current
+    res.image = 'data:image/png;base64,' + res.image
+    return new Promise(function (resolve, reject) {
+      resemble(res.image).compareTo(image).scaleToSameSize().onComplete(function (data) {
+        var result = false
+        if (parseFloat(data.misMatchPercentage) <= misMatchPercentageMargin) {
+          // Passed
+          $.ajax({
+            type: 'POST',
+            async: false,
+            url: '/passed',
+            data: {
+              image: image,
+              name: browserDirectory + imageName + '.png'
+            }
+          })
+          result = true
+          node.innerHTML = '<div class="test pass"> <div class="list-name">  New: ' + imageName + '</div> <div class="additional-info"> Addition Information: </div> <img src="' + image + '" /> </div>'
+        } else {
+          // Fail
+          $.ajax({
+            type: 'POST',
+            async: false,
+            url: '/fail',
+            data: {
+              image: data.getImageDataUrl(),
+              name: browserDirectory + imageName + '.png'
+            }
+          })
+          node.innerHTML = '<div class="test fail"> <div class="list-name">  Changed: ' + imageName + ' </div> <div class="additional-info"> Addition Information: </div> <div class="images"> <div class="image"> <img class="diff" src="' + data.getImageDataUrl() + '" /> <div class="caption">  Diff   </div> </div> <div class="image">  <img class="input" src="' + image + '" /> <div class="caption"> Current  </div> </div> <div class="image"> <img class="passed" src="' + res.image + '" /> <div class="caption"> Baseline   </div> </div> </div> </div>'
 
-            images.push(data.getImageDataUrl())
-            images.push(image)
-            images.push(res.image)
-            $.ajax({
-              type: 'POST',
-              async: false,
-              url: '/report',
-              data: {
-                type: 'Changed',
-                images: images,
-                name: imageName
-              }
-            })
-          }
-          $(document.getElementById('ember-testing')).removeAttr('style')
-          $(document.getElementById('ember-testing-container')).removeAttr('style')
-          document.getElementsByClassName('visual-acceptance-container')[0].appendChild(node)
-          assert = assert === undefined ? chai.assert : assert
-          assert.equal(result, true, 'Image mismatch percentage (' + data.misMatchPercentage + ') is above mismatch threshold(' + misMatchPercentageMargin + ').')
-          data ? resolve(data) : reject(data)
-        })
-      }).then(function (data) {
+          images.push(data.getImageDataUrl())
+          images.push(image)
+          images.push(res.image)
+          $.ajax({
+            type: 'POST',
+            async: false,
+            url: '/report',
+            data: {
+              type: 'Changed',
+              images: images,
+              name: imageName
+            }
+          })
+        }
+        $(document.getElementById('ember-testing')).removeAttr('style')
+        $(document.getElementById('ember-testing-container')).removeAttr('style')
+        document.getElementsByClassName('visual-acceptance-container')[0].appendChild(node)
+        assert = assert === undefined ? chai.assert : assert
+        assert.equal(result, true, 'Image mismatch percentage (' + data.misMatchPercentage + ') is above mismatch threshold(' + misMatchPercentageMargin + ').')
         data ? resolve(data) : reject(data)
       })
-    }
-  })
+    }).then(function (data) {
+      data ? resolve(data) : reject(data)
+    })
+  }
 }
