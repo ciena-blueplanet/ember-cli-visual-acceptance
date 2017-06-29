237714c83e3de6feff10356d472a74ca118ae5fb
Eric White <ewhite@ciena.com>
2017-06-29 12:11:55 -0400

    fix dependencies

--- blueprints/ember-cli-visual-acceptance/index.js
+++ blueprints/ember-cli-visual-acceptance/index.js
@@ -4,21 +4,10 @@ module.exports = {
   normalizeEntityName: function () {
     // no-op
   },
-
   afterInstall: function (options) {
     // Perform extra work here.
     return this.insertIntoFile('.gitignore', '/visual-acceptance').then(function (params) {
-      return this.insertIntoFile('.npmignore', '/visual-acceptance').then(function () {
-        return this.addPackagesToProject([
-          {name: 'body-parser', target: '^1.15.1'},
-          {name: 'chalk', target: '^1.1.3'},
-          {name: 'es6-promise', target: '^3.2.2'},
-          {name: 'nightmare', target: '^2.10.0'},
-          {name: 'nightmare-custom-event', target: '^0.2.0'},
-          {name: 'resemblejs', target: '2.2.0'},
-          {name: 'sync-request', target: '^3.0.1'}
-        ])
-      }.bind(this))
+      return this.insertIntoFile('.npmignore', '/visual-acceptance')
     }.bind(this))
   }
 }
